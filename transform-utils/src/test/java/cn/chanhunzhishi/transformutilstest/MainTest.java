c7f28aa5d53bf654703ed6f6c437ff1b625d0fca
JHL <jiaohonglin@vip.qq.com>
2018-06-25 16:24:54 +0800

    初始化项目

--- transform-utils/src/test/java/cn/chanhunzhishi/transformutilstest/MainTest.java
+++ transform-utils/src/test/java/cn/chanhunzhishi/transformutilstest/MainTest.java
@@ -2,6 +2,6 @@ package cn.chanhunzhishi.transformutilstest;
 
 public class MainTest {
 	public static void main(String[] args) {
-		System.out.println("HELLO WORD");
+		System.out.println("HELLO WORD!");
 	}
 }
